707c33b7800a3d4a516bbd48f0ecc87c885487a8
Daniil Suvorov <dannil.suvorov.98@gmail.com>
2026-05-16 10:42:47 +0300

    подставляет лучшие параметры в модель

--- backend/train_sentiment_model.py
+++ backend/train_sentiment_model.py
@@ -1,3 +1,4 @@
+import re
 import os
 import time
 import joblib
@@ -18,60 +19,56 @@ def clean_text(text: str) -> str:
 
     text = text.lower()  # lowercase
     text = re.sub(r"http\S+", "", text)  # убрать ссылки
-    text = re.sub(r"@\w+", "", text)     # убрать @username
-    text = re.sub(r"[^a-z\s]", "", text) # убрать всё кроме букв
+    text = re.sub(r"@\w+", "", text)  # убрать @username
+    text = re.sub(r"[^a-z\s]", "", text)  # убрать всё кроме букв
     text = re.sub(r"\s+", " ", text).strip()  # лишние пробелы
     return text
 
+
 start = time.time()
+print("Старт обучения модели.\n")
 
 os.makedirs("models", exist_ok=True)
 
 feature_names = ["target", "id", "date", "flag", "user", "text"]
 
 df = pd.read_csv(
-    "./data/Sentiment140.csv",
-    sep=",",
-    encoding="latin-1",
-    names=feature_names
+    "./data/Sentiment140.csv", sep=",", encoding="latin-1", names=feature_names
 )
 
 df["target"] = df["target"].replace({4: 1})
 df["text_clean"] = df["text"].apply(clean_text)
 
 X_train, X_test, y_train, y_test = train_test_split(
-    df["text_clean"],
-    df["target"],
-    stratify=df["target"],
-    test_size=0.3,
-    random_state=1
+    df["text_clean"], df["target"], stratify=df["target"], test_size=0.3, random_state=1
 )
 
-# Сюда вставляешь лучшие параметры из search.best_params_
+# Модель
 model = Pipeline(
     [
-        (
-            "tfidf",
-            TfidfVectorizer(
-                sublinear_tf=True,
-                analyzer="word",
-                ngram_range=(1, 2),
-                min_df=3,
-                max_features=200_000,
-            ),
-        ),
+        ("tfidf", TfidfVectorizer(sublinear_tf=True)),
         (
             "logreg",
             LogisticRegression(
                 max_iter=1000,
                 solver="saga",
-                C=2.0,
-                penalty="l2",
                 n_jobs=-1,
             ),
         ),
     ]
 )
+
+# Лучшие параметры
+best_params = {
+    "tfidf__ngram_range": (1, 2),
+    "tfidf__min_df": 3,
+    "tfidf__max_features": 200_000,
+    "tfidf__analyzer": "word",
+    "logreg__penalty": "l2",
+    "logreg__C": 1.0,
+}
+
+model.set_params(**best_params)
 
 model.fit(X_train, y_train)
 
